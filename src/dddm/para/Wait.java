76eea27c14c57b93a47b26dba3cdb3f194f0e749
qiujiamin <qiujiamin@diandian-tech.com>
2017-11-29 14:42:10 +0800

    date time :2017-11-29 last update user:jasmine section:comments

--- src/dddm/para/Wait.java
+++ src/dddm/para/Wait.java
@@ -12,16 +12,18 @@ public class Wait {
 	public Wait(WebDriver driver){
 		this.driver = driver;
 	}
+	//等待元素可操作
 	public void waitForElementPresents(String locator){
 		try{(new WebDriverWait(driver,timeout)).until(ExpectedConditions.presenceOfElementLocated(By.xpath(locator)));
 	}catch(Exception e){
 		
 	 }
   }
-	
+	//等待元素可启用
 	public void waitForElementEnable(String locator){
 		(new WebDriverWait(driver,timeout)).until(ExpectedConditions.elementToBeClickable(By.xpath(locator)));
 	}
+	//直接等待几秒
 	public void waitFor(long timeout){
 		try {
 			Thread.sleep(timeout);
